10fad5ea3b0da382b99961c486ef9fc580f34806
uniqquej <cjy4477@gmail.com>
2024-05-28 11:59:01 +0900

    objectMapper custom

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,14 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -13,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountApiController {
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+        return AccountMeResponse.builder()
+                .name("테스터")
+                .email("A@A.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
