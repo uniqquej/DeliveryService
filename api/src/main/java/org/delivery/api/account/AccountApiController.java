37c6fa0fef7ff9a0514ebaccf248328ccfb7b186
uniqquej <cjy4477@gmail.com>
2024-05-28 13:22:06 +0900

    error code 설정

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,6 +3,7 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +19,13 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public Api<AccountMeResponse> me(){
+    public Api<Object> me(){
         var response = AccountMeResponse.builder()
                 .name("테스터")
                 .email("A@A.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
-        return Api.OK(response);
+
+        return Api.ERROR(UserErrorCode.USER_NOT_FOUND,"테스터 사용자 없음");
     }
 }
--- api/src/main/java/org/delivery/api/common/api/Api.java
+++ api/src/main/java/org/delivery/api/common/api/Api.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 @Data
 @NoArgsConstructor
@@ -18,6 +19,30 @@ public class Api<T> {
         var api = new Api<T>();
         api.body = data;
         api.result = Result.OK();
+        return api;
+    }
+
+    public static Api<Object> ERROR(Result result){
+        var api = new Api<Object>();
+        api.result = result;
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs, Throwable tx){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs,tx);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs, String description){
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs,description);
         return api;
     }
 
