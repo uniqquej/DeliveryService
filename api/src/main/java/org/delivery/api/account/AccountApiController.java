96def0b09117ee06f149e7cc6edce76181bff4a2
uniqquej <cjy4477@gmail.com>
2024-05-28 13:50:05 +0900

    exception handler 추가

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,7 +3,8 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
-import org.delivery.api.common.error.UserErrorCode;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +20,21 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public Api<Object> me(){
+    public Api<AccountMeResponse> me(){
         var response = AccountMeResponse.builder()
                 .name("테스터")
                 .email("A@A.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
 
-        return Api.ERROR(UserErrorCode.USER_NOT_FOUND,"테스터 사용자 없음");
+        var str = "hi";
+        var age = 0;
+        try{
+            Integer.parseInt(str);
+        }catch (Exception e){
+            throw new ApiException(ErrorCode.SERVER_ERROR, e, "사용자 me 호출 에러 발생");
+        }
+
+        return Api.OK(response);
     }
 }
