b02dd13c258b029b1bfb3ddf25dbf563aff953cd
uniqquej <cjy4477@gmail.com>
2024-05-31 16:35:35 +0900

    cors 설정

--- api/src/main/java/org/delivery/api/config/web/WebConfig.java
+++ api/src/main/java/org/delivery/api/config/web/WebConfig.java
@@ -5,6 +5,7 @@ import org.delivery.api.interceptor.AuthorizationInterceptor;
 import org.delivery.api.resolver.UserSessionResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -38,5 +39,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userSessionResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                ;
     }
 }
