9d462db098a0e42b7c6427a69bca09a685c6b86d
uniqquej <cjy4477@gmail.com>
2024-06-20 02:55:24 +0900

    fix: 가맹점 - home css 수정

--- store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/HomeController.java
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/controller/HomeController.java
@@ -1,12 +1,19 @@
 package org.delivery.storeadmin.domain.userorder.controller;
 
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping(path = {"","/"})
-    public String home(){
+    public String home(
+            @AuthenticationPrincipal UserSession userSession,
+            Model model
+            ){
+        model.addAttribute("storeName", userSession.getStoreName());
         return "home";
     }
 }
